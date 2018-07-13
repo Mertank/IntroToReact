4fdfa6a520a993f26797d98c1c9b59cffe6dfc1c
Karl Merten <karlmer10@gmail.com>
2018-07-13 19:21:23 -0400

    Added video display

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = { videos: [] };
+        YTSearch({ key: process.env.YOUTUBE_KEY, term: 'Aaron Ross' }, (videos) => { this.setState({ videos }); })
     }
 
     render() {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
